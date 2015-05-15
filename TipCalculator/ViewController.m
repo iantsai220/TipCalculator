59f38e4d1a1c62203ba7ee1b6807bf0cc62229da
iantsai <iantsai86@gmail.com>
2015-05-15 10:03:04 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TipCalculator
+//
+//  Created by Ian Tsai on 2015-05-15.
+//  Copyright (c) 2015 Ian Tsai. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
